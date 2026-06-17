eadc2d04ce76320048fb5d4ad1ba16c3c017b699
Busakrish <busakrish7237@gmail.com>
2026-06-17 08:39:45 +0530

    i created main class and create a database table

---
+++ src/main/java/com/nexus/nexus/Main.java
@@ -0,0 +1,7 @@
+package com.nexus.nexus;
+import com.nexus.nexus.db.DatabaseManager;
+public class Main {
+    public static void main(String[] args){
+        DatabaseManager.createTables();
+    }
+}
